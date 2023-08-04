e9349d8103f1e54913dc7de30455307dac3f9418
rohit nohwar <rohit.nohwar@gmail.com>
2023-08-04 23:15:55 +0530

    first commit

--- lld/Patterns/CreationalPatterns/BuilderPattern/BuilderPatternImplementaion2/src/Client.java
+++ lld/Patterns/CreationalPatterns/BuilderPattern/BuilderPatternImplementaion2/src/Client.java
@@ -1,2 +1,11 @@
 public class Client {
+    private static UserDto directBuild(UserDto.UserDtoBuilder builder, String firstName, String lastName, int age) {
+        return builder.fillFirstName(firstName).fillLastName("Nohwar").fillAge(23).build();
+    }
+    public static void main(String args[]) {
+        UserDto userDtoCreatedByBuilderPattern = directBuild(UserDto.getBuilder(), "Rohit", "Nohwar", 23);
+        System.out.println("User first name:- "+userDtoCreatedByBuilderPattern.getFirstName());
+        System.out.println("User last name:- "+userDtoCreatedByBuilderPattern.getLastName());
+        System.out.println("User age:- "+userDtoCreatedByBuilderPattern.getAge());
+    }
 }
--- lld/Patterns/CreationalPatterns/BuilderPattern/BuilderPatternImplementaion2/src/UserDto.java
+++ lld/Patterns/CreationalPatterns/BuilderPattern/BuilderPatternImplementaion2/src/UserDto.java
@@ -8,31 +8,47 @@ public class UserDto {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public static UserDtoBuilder getBuilder() {
+        return new UserDtoBuilder();
     }
 
-    public static class userDtoBuilder {
+    public static class UserDtoBuilder {
         private String firstName;
         private String lastName;
         private int age;
 
+        private UserDto userDto = new UserDto();
 
+        public UserDtoBuilder fillFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserDtoBuilder fillLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserDtoBuilder fillAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public UserDto build() {
+            userDto.firstName = this.firstName;
+            userDto.lastName = this.lastName;
+            userDto.age = this.age;
+            return userDto;
+        }
     }
 }
+
+
