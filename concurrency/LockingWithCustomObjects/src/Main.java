75003a932e4b6a819e799c53b88f89af97852a27
rohit nohwar <rohit.nohwar@gmail.com>
2023-08-09 02:31:43 +0530

    first commit

--- concurrency/LockingWithCustomObjects/src/Main.java
+++ concurrency/LockingWithCustomObjects/src/Main.java
@@ -1,13 +1,27 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        Execute execute = new Execute();
+    public static int counter1 = 0;
+    public static int counter2 = 0;
+    public static final Object lock1 = new Object();
+    public static final Object lock2 = new Object();
 
+    public static void increment1() {
+        synchronized (lock1) {
+            counter1++;
+        }
+    }
+
+    public static void increment2() {
+        synchronized (lock2) {
+            counter2++;
+        }
+    }
+    public static void main(String[] args) {
         Thread t1 = new Thread(){
             public void run(){
                 for (int i = 0; i < 10000; i++) {
-                    execute.increment();
+                    increment1();
                 }
             }
         };
@@ -15,7 +29,7 @@ public class Main {
         Thread t2 = new Thread(){
             public void run(){
                 for (int i = 0; i < 10000; i++) {
-                    execute.increment();
+                    increment2();
                 }
             }
         };
@@ -35,16 +49,7 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-        System.out.println(execute.counter);
-    }
-}
-
-class Execute {
-    public int counter = 0;
-
-    public void increment() {
-        synchronized (this) {
-            counter++;
-        }
+        System.out.println("counter1:- "+counter1);
+        System.out.println("counter2:- "+counter2);
     }
 }
