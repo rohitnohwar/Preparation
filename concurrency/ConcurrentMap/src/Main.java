6caaafba34f50805b77644a21affb1f91713b71f
rohit nohwar <rohit.nohwar@gmail.com>
2023-09-08 00:33:40 +0530

    first commit

---
+++ concurrency/ConcurrentMap/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.PriorityBlockingQueue;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        ConcurrentMap<Integer, Integer> map= new ConcurrentHashMap<>();
+
+        Thread t1 = new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    map.put(71, 23);
+                    System.out.println("added");
+                    Thread.sleep(1000);
+                    map.put(922, 23);
+                    System.out.println("added");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                //                    Thread.sleep(10);
+                map.remove(71);
+                System.out.println("taken");
+//                    Thread.sleep(5000);
+                map.remove(922);
+                System.out.println("taken");
+            }
+        };
+
+        t1.start();
+        t2.start();
+    }
+}
