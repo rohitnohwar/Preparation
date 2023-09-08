f63347feb5fcdd8cd245d9902e105a1e0ed525a2
rohit nohwar <rohit.nohwar@gmail.com>
2023-09-09 03:52:20 +0530

    first commit

--- concurrency/InterruptCurrentThread/src/Main.java
+++ concurrency/InterruptCurrentThread/src/Main.java
@@ -4,36 +4,19 @@ import java.util.concurrent.TimeUnit;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-class Task implements Runnable {
-    @Override
-    public void run() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-
-        System.out.println(Thread.currentThread().getName());
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        ExecutorService executer = Executors.newFixedThreadPool(3);
-
-        for (int i = 0; i < 10; i++) {
-            executer.execute(new Task());
-        }
-
-        // Prevents executor from executing further tasks. The previously executed ones will keep running.
-        executer.shutdown();
-
-        try {
-            if (!executer.awaitTermination(100, TimeUnit.MILLISECONDS)) {
-                executer.shutdownNow();
+        Thread t1 = new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException("Thread interrupted");
+                }
             }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        };
+
+        t1.start();
+        t1.interrupt();
     }
 }
