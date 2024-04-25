8cacc6ca11f9257664ce7e4d4c977d7c2ced3a9f
Rohit Nohwar <rohit.nohwar@gmail.com>
2024-04-25 05:34:36 +0530

    sjkd

--- misc/QuickSort/src/Main.java
+++ misc/QuickSort/src/Main.java
@@ -1,17 +1,43 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+class QuickSort {
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    private static int partition(int[] arr, int low, int high) {
+        int i = low;
+        int j = high;
+        
+        int pivot = arr[low];
+        
+        while (i < j) {
+            while (i <= high && arr[i] <= pivot) i++;
+            while (j >= low && arr[j] > pivot) j--;
+            
+            if (i < j) swap(arr, i, j);
+        }
+        
+        swap(arr, low, j);
+        
+        return j;
+    }
+    public static void quicksort(int[] arr, int low, int high) {
+        if (low < high) {
+            int partition = partition(arr, low, high);
+
+            quicksort(arr, low, partition - 1);
+            quicksort(arr, partition + 1, high);
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr = { 13, 18, 27, 2, 19, 25 };
+        QuickSort.quicksort(arr, 0, arr.length - 1);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + ", ");
     }
 }
