4940c625bdc9d2e7c5298ffca899359df7bd56c3
rohit.nohwar <rohit.nohwar@gmail.com>
2025-06-16 09:12:16 +0530

    wd

--- concurrency/WaitAndNotifyAll/src/Main.java
+++ concurrency/WaitAndNotifyAll/src/Main.java
@@ -32,6 +32,27 @@ class Waiter2 extends Thread
         }
     }
 }
+
+class Waiter3 extends Thread
+{
+    public Waiter1 waiter1;
+    Waiter3(Waiter1 waiter1) {
+        this.waiter1 = waiter1;
+    }
+
+    public void run() {
+        synchronized (this.waiter1) {
+            System.out.println("In Waiter3 before wait()");
+            try {
+                this.waiter1.wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("In Waiter3 after this.waiter1.wait()");
+        }
+    }
+}
+
 class Notifier extends Thread {
     public Waiter1 waiter1;
     Notifier(Waiter1 waiter1) {
@@ -58,10 +79,12 @@ public class Main
     {
         Waiter1 waiter1 = new Waiter1();
         Waiter2 waiter2 = new Waiter2(waiter1);
+        Waiter3 waiter3 = new Waiter3(waiter1);
         Notifier notifier = new Notifier(waiter1);
 
         waiter1.start();
         waiter2.start();
+        waiter3.start();
         Thread.sleep(1000);
         notifier.start();
     }
